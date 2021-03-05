1711be103a882cba3e5df4086bde70cefea997f3
Iree Hale <LightWatch21@protonmail.com>
2021-03-05 16:23:02 -0700

    fixing project files

--- Todo.js
+++ Todo.js
@@ -1,1 +1,7 @@
-console.log("I'm working!");
+let addBtn = document.getElementById("add");
+let input = document.getElementById("search");
+let list = document.getElementById("mylist");
+
+addBtn = document.addEventListener('click', function (){
+    console.log("added");
+});
