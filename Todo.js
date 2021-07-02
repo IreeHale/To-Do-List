cea5c717587538df0a01263817187eb0ab2e5bb9
Iree Hale <LightWatch21@protonmail.com>
2021-07-02 09:45:12 -0700

    rebuilding javascript and css components

---
+++ Todo.js
@@ -0,0 +1,18 @@
+let addbutton = document.getElementById('addbtn');
+let todocontainer = document.getElementById('todocontainer');
+let inputfield = document.getElementById('input');
+
+addbutton.addEventListener('click', function() {
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph.styling');
+    paragraph.innerText = input.value;
+    todocontainer.appendChild(paragraph);
+    input.value = '';
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = 'line through';
+    })
+    
+    paragraph.addEventListener('dbclick', function(){
+        todocontainer.removeChild(paragraph);
+    })
+})
