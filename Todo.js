cc008fddf034130f87126fe1a4a9cae12c6f0d6c
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2021-12-22 02:34:15 -0500

    added styles to second container and added info for js file

--- Todo.js
+++ Todo.js
@@ -1,2 +1,4 @@
 //create variable connected to the class items in the index file
-//
+//create add btn functionality to add to do items
+//create line through functionality for the todo when clicked
+//when you want to delete something you dble click it to remove it
