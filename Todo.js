ac9027a72dba7729b4e59bf0a9413ee0ff479c20
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2021-12-23 12:03:06 -0500

    fixing js code and styling search bar

--- Todo.js
+++ Todo.js
@@ -1,7 +1,7 @@
 //create variable connected to the class items in the index file
-input = document.getElementById("search");
-add = document.getElementById("add");
-container = document.getElementsByClassName("container2");
+let input = document.getElementById("search");
+let add = document.getElementById("add");
+let container = document.getElementsByClassName("container2");
 //create add btn functionality to add to do items
 //create line through functionality for the todo when clicked
 //when you want to delete something you dble click it to remove it
