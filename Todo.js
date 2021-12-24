26dc0e1c34fe7c2b770aa9de893e8e4638505c63
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2021-12-23 21:40:32 -0500

    working on the funcionality of the add button

--- Todo.js
+++ Todo.js
@@ -3,5 +3,9 @@ let input = document.getElementById("search");
 let add = document.getElementById("add");
 let container = document.getElementsByClassName("container2");
 //create add btn functionality to add to do items
+add.addEventListener("click", function(){
+    let paragraph = document.CreateElement('p');
+    console.log(input.value);
+});
 //create line through functionality for the todo when clicked
 //when you want to delete something you dble click it to remove it
