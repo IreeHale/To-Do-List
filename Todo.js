24573637abcf33e1aa1d5b6a8ccc8b66eb643f58
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2021-12-23 22:55:48 -0500

    fixing bug for add btn

--- Todo.js
+++ Todo.js
@@ -1,11 +1,12 @@
 //create variable connected to the class items in the index file
 let input = document.getElementById("search");
 let add = document.getElementById("add");
-let container = document.getElementsByClassName("container2");
+let container = document.getElementById("container2");
 //create add btn functionality to add to do items
-add.addEventListener("click", function(){
-    let paragraph = document.CreateElement('p');
-    console.log(input.value);
+add.addEventListener("click", function addToContainer(){
+    let p = document.createElement('p');
+    p.innerText = input.value;
+    container.appendChild(p);
 });
 //create line through functionality for the todo when clicked
 //when you want to delete something you dble click it to remove it
