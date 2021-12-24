b951ad1b3f3b0736fbf0e213e6670b2c923b13bd
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2021-12-24 01:08:21 -0500

    adding conditional statement and will be working on removal section

--- Todo.js
+++ Todo.js
@@ -5,8 +5,12 @@ let container = document.getElementById("container2");
 //create add btn functionality to add to do items
 add.addEventListener("click", function addToContainer(){
     let p = document.createElement('p');
-    p.innerText = input.value;
+    if ( input.value === "") {
+        alert("add a To-Do please.");
+    }else{
+        p.innerText = input.value;
     container.appendChild(p);
+    }
 });
 //create line through functionality for the todo when clicked
 //when you want to delete something you dble click it to remove it
