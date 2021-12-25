450135b656fc3df3597585e74d184877e71c7e9a
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2021-12-25 02:57:41 -0500

    created line through functionality and delete functionality

--- Todo.js
+++ Todo.js
@@ -4,13 +4,22 @@ let add = document.getElementById("add");
 let container = document.getElementById("container2");
 //create add btn functionality to add to do items
 add.addEventListener("click", function addToContainer(){
-    let p = document.createElement('p');
+    const p = document.createElement('p');
     if ( input.value === "") {
         alert("add a To-Do please.");
     }else{
         p.innerText = input.value;
         container.appendChild(p);
     }
+
+    p.addEventListener("click", function lineThrough(){
+        p.style.textDecoration = "line-through"
+    });
+
+    p.addEventListener("dblclick", function removeItem(){
+        container.removeChild(p);
+    })
 });
+
 //create line through functionality for the todo when clicked
 //when you want to delete something you dble click it to remove it
